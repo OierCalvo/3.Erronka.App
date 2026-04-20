5ad9c160d5083a387d9c3819876dfd8880fe2f66
Aimar <aim.zab.jau@goierrieskola.org>
2026-04-20 22:23:33 +0200

    interfazeen konfigurazioa osatu

--- 3Erronka/interfazeLogin.cs
+++ 3Erronka/interfazeLogin.cs
@@ -18,29 +18,43 @@ namespace _3Erronka
             {
                 MessageBox.Show("Admina zara", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 interfazeAdmin ia = new interfazeAdmin();
-                
                 ia.Show();
                 this.Hide();
             }
             else if(l.getRola().Equals("kordinatzailea"))
             {
                 MessageBox.Show("Kordinatzailea zara", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                interfazeAdmin ia = new interfazeAdmin();
+                ia.Show();
+                this.Hide();
             }
             else if (l.getRola().Equals("albaitaria"))
             {
                 MessageBox.Show("Albaitaria zara", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                interfazeAdmin ia = new interfazeAdmin();
+                ia.Show();
+                this.Hide();
             }
             else if (l.getRola().Equals("zaintzaile_arrunta"))
             {
                 MessageBox.Show("Zaintzaile arrunta zara", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                interfazeAdmin ia = new interfazeAdmin();
+                ia.Show();
+                this.Hide();
             }
             else if (l.getRola().Equals("zaintzailea_nagusia"))
             {
                 MessageBox.Show("Zaintzaile nagusia zara", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                interfazeAdmin ia = new interfazeAdmin();
+                ia.Show();
+                this.Hide();
             }
             else
             {
                 MessageBox.Show("Erabiltzailea edo pasahitza ez dira zuzenak", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                interfazeAdmin ia = new interfazeAdmin();
+                ia.Show();
+                this.Hide();
             }
         }
     }
